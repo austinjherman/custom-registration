aec72bcd153f2ecea085990da3a1f5474e175fd3
Austin Herman <aherman@sociusmarketing.com>
2019-01-14 14:08:22 -0500

    set up stripe integration

--- florida-swim-custom-registration.php
+++ florida-swim-custom-registration.php
@@ -26,7 +26,7 @@ $dotenv->load();
 const STRIPE_TEST_KEY_SECRET = getenv('STRIPE_TEST_KEY_SECRET');
 const STRIPE_LIVE_KEY_SECRET = getenv('STRIPE_LIVE_KEY_SECRET');
 
-var stripe = new \stdClass();
+$stripe = new \stdClass();
 $stripe->testKey = STRIPE_TEST_KEY_SECRET;
 $stripe->liveKey = STRIPE_LIVE_KEY_SECRET;
 
